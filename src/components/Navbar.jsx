5167732c6926a0b350f1454a5f358fca6ea22047
Victoria Canclini <vico_canclini@hotmail.com>
2024-09-04 14:45:35 -0300

    ok styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
       );
       Cookies.remove("token");
       dispatch(setCredentials({ user: null, email: "" }));
-      router.push("/");
+      router.push("/login");
     } catch (error) {
       console.error("Error al cerrar sesión:", error);
     }
@@ -29,14 +29,14 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between bg-black h-[40px] sm:h-[50px]">
-      <div className="mt-6 ml-2">
+      <div className="mt-7 ml-2">
         <img src="/img/Logo 01.png" alt="Logo1" width={70} />
       </div>
       <div className="flex justify-end gap-2 sm:mt-6 mr-4 mt-7">
         {email ? (
           <button
             onClick={handleLogout}
-            className="bg-[#C1FD35] text-black rounded-md pl-4 pr-4 pt-2 pb-2"
+            className="bg-[#C1FD35] text-black rounded-md pt-5 pb-5 pl-3 pr-3 flex items-center"
           >
             Cerrar sesión
           </button>

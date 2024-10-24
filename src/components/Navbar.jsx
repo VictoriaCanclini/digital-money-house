b45b7d9a593898d586c73581aaf6eecd9e417e37
Victoria Canclini <vico_canclini@hotmail.com>
2024-10-24 14:23:03 -0300

    ok styles3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -65,15 +65,15 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between bg-black h-[60px]">
-      <div className="mt-5 ml-2">
-        <Link href="/">
+      <div className="mt-8 md:mt-5 ml-2">
+        <Link href="/login">
           <Image src="/img/Logo 01.png" alt="Logo1" width={70} height={70} />
         </Link>
       </div>
       <div className="flex justify-end gap-2 sm:mt-4 mr-4 mt-5 sm:mb-2">
         {isClient && email ? (
           <>
-            <button className="bg-[#C1FD35] text-black rounded-lg pt-4 pb-4 pl-3 pr-3 mb-1 flex items-center">
+            <button className="bg-[#C1FD35] text-black rounded-lg pt-4 pb-4 pl-3 pr-3 mb-1 flex items-center mt-2 md:mt-1">
               <Link href={`/profile/${user_id}`}>{initials}</Link>
             </button>
             <p className="hidden sm:block mt-3">
@@ -111,7 +111,7 @@ const Navbar = () => {
       </div>
       {isMenuOpen && (
         <div className="absolute top-[60px] right-0 bg-[#C1FD35] bg-opacity-70 text-black w-[30%] rounded-md sm:hidden">
-          <ul className="flex flex-col items-center gap-4 p-4">
+          <ul className="flex flex-col gap-4 p-4">
             <li>
               <Link href="/home" onClick={toggleMenu}>
                 Inicio
